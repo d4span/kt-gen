b0fb3fad4e25b508b26c55eae3de2843eb7b9e53
David Caspar <david.caspar@aol.ch>
2019-09-22 10:47:01 +0200

    Added build files.

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.50"
+}
+
+group = "org.kcheck"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+
+    testImplementation(kotlin("test"))
+    testImplementation(kotlin("test-junit"))
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
